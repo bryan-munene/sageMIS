574936a8998bbceecc287b29ac83dbc0f0db5fb8
Slowcoach <knkarithi@gmail.com>
2014-02-08 19:06:15 +0300

    added the sample users

--- install.sql
+++ install.sql
@@ -1,9 +1,14 @@
-CREATE USER sage IDENTIFIED BY '!sage';
 
 CREATE DATABASE pharm_engine_test CHARACTER SET utf8 COLLATE utf8_general_ci;
 CREATE DATABASE pharm_engine_development CHARACTER SET utf8 COLLATE utf8_general_ci;
 CREATE DATABASE pharm_engine_production CHARACTER SET utf8 COLLATE utf8_general_ci;
 
-GRANT all privileges ON pharm_engine_development.* TO 'sage'@'localhost' IDENTIFIED BY '!sage';
-GRANT all privileges ON pharm_engine_production.* TO 'sage'@'localhost' IDENTIFIED BY '!sage';
-GRANT all privileges ON pharm_engine_test.* TO 'sage'@'localhost' IDENTIFIED BY '!sage';
+use pharm_engine_development;
+
+INSERT INTO `users` (`id`, `first_name`, `last_name`, `user_name`, `password`, `email`, `salt`, `is_locked`, `archived`, `created_at`, `updated_at`, `hashed_password`, `role_id`, `is_first_time`, `failed_attempts`) VALUES
+(1, 'Test', 'User', 'TUser', NULL, 'tuser@email.com', NULL, NULL, NULL, '2014-02-02 14:42:14', '2014-02-02 14:42:14', NULL, NULL, NULL, NULL),
+(2, 'sample', 'sample', 'sample', NULL, 'sample@email.com', 'V8MndoiFXo', NULL, NULL, '2014-02-08 10:07:30', '2014-02-08 10:07:30', 'f2a182d284d8f5ad5d71e213eece14c76242a654', NULL, NULL, NULL),
+(3, 'favourite', 'admin', 'msieu', NULL, 'msieu@sage.co.ke', 'HNpXGrjClc', NULL, NULL, '2014-02-08 10:35:20', '2014-02-08 10:35:20', 'e331f83d5379d4363d3f91b53cbd2da890da311e', NULL, NULL, NULL),
+(4, 'another', 'users', 'robot', NULL, 'robot@sage.co.ke', 'g7PQColOOu', NULL, NULL, '2014-02-08 10:52:11', '2014-02-08 10:52:11', '28f3c4e84200ae1031d185ae350c567e69b694a4', NULL, NULL, NULL),
+(5, 'Rick', 'Dees', 'rickd', NULL, 'rickd@rick.com', 'TJuqDkaNUu', NULL, NULL, '2014-02-08 12:11:40', '2014-02-08 13:27:52', '09c19a9e42d55137fdeea7c834b0bdedf863fe47', NULL, 0, 0),
+(6, 'sage_admin', 'sage_admin', 'sage_admin', NULL, 'sage@sage.com', '8AMf5klxLL', NULL, NULL, '2014-02-08 16:00:48', '2014-02-08 16:00:48', 'f91a4b8daca77ea0a4653c11612635701e60701e', NULL, 1, 0);
