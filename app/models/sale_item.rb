d5851d41f727b168298595f0bbdea34614d3cf7d
slowcoach <knkarithi@gmail.com>
2014-07-29 19:11:26 +0300

    Floating point to two decimal places

--- app/models/sale_item.rb
+++ app/models/sale_item.rb
@@ -3,21 +3,22 @@ class SaleItem < ActiveRecord::Base
   def newitem(args)
     #code
     item_name = args[:item_id]
-    quantity = (args[:quantity]).blank?? 0 : (args[:quantity])
+    quantity = (args[:quantity]).blank?? 0.to_f : (args[:quantity]).to_f
     
     Rails.logger.debug "#{quantity}"
     #use the item name to get the id of the item from the Items table
     @item = Item.where("item_name like ? ",item_name).first
     if @item
     #calculate the line cost and include the sale constant for an ongoing sale
-    linecost = quantity * @item.adjusted_price
+    linecostm = quantity.to_f * @item.adjusted_price.to_f
+    
     Rails.logger.debug "#{linecost}"
     #assign the variables to the instance
     
     self.item_id = @item.id
     self.sale_id = 9999999999.to_i
     self.quantity = quantity.to_f.round(2)
-    self.linecost = linecost.to_f.round(2)
+    self.linecost = linecostm.round(2)
   end
   end
   def self.in_progress
