94e52a59933e158c3e0726e56fcbaa8d8ac0d2db
slowcoach <knkarithi@gmail.com>
2014-07-29 15:23:27 +0300

    Making the requested changes

--- app/models/batch.rb
+++ app/models/batch.rb
@@ -1,4 +1,8 @@
 class Batch < ActiveRecord::Base
   belongs_to :item
-  attr_accessible :date, :description, :name, :no_of_items,:expiry_date,:batch_no,:creator
+  attr_accessible :date, :description, :name, :no_of_items,:expiry_date,:batch_no,:creator,:remaining_items
+  before_create :set_remaining_items
+  def set_remaining_items
+    self.remaining_items = self.no_of_items
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,27 +1,42 @@
 class Item < ActiveRecord::Base
   include Codegen
   attr_accessible :adjusted_price, :buying_price, :calculated_selling, :custom_attribute, :custom_size, :item_attribute, :item_code, :item_name, :markup, :size, :source, :tax_class, :warehouse,:item_description,:dosage,:manufacturer,:original_price,:cvs_import,:old_item_number,:creator
-
-  before_validation :capitalize_item_name,:default_space_values,:create_selling_price
+  before_validation :capitalize_item_name,:default_space_values,:create_markup,:create_selling_price
   after_save :generate_code,:generate_default_batch
  # validates :item_name,:size,:tax_class,:warehouse,:presence=>true
   validates_length_of :item_name, :minimum => 3
-  validates_numericality_of :adjusted_price,:buying_price,:calculated_selling,:markup
+  validates_numericality_of :adjusted_price,:buying_price,:calculated_selling
   validate :price_is_sensible
+  validate :adjusted_price_sensible
   has_many :batches
 
 
   def price_is_sensible
     #miscellaneous price checks here
     if self.buying_price == 0  and self.markup==0
-      errors.add("Price","cannot be zero")
+      errors.add("Buying Price","cannot be zero")
+    end
+
+  end
+  def mark_up_is_sensible
+    if self.markup.to_f == 0
+      errors.add("Markup","is not numeric")
+    end
+  end
+  
+  def adjusted_price_sensible
+    #miscellaneous price checks here
+   # Rails.logger.debug "#{self.inspect}"
+    if self.adjusted_price.to_i == 0 
+      #errors.add("Selling Price","cannot be zero")
+      self.adjusted_price = self.calculated_selling.to_f
     end
 
   end
   def default_space_values
       #set all non created params to zero or nil
-
     if self.adjusted_price.nil?
+      
        self.adjusted_price = 0
     end
     if self.markup.nil?
@@ -35,10 +50,21 @@ class Item < ActiveRecord::Base
       #use the first tax class as the default
          self.tax_class = 1
     end
-    tax = Taxcode.find(self.tax_class.to_i)
-    percent_amt = tax.percentage_markup.to_f
+    #ignore tax things for the moment
+    #tax = Taxcode.find(self.tax_class.to_i)
+    #percent_amt = tax.percentage_markup.to_f
    price = (self.buying_price.to_f + self.markup.to_f)
-    self.calculated_selling = (1+(percent_amt/100))*price
+    self.calculated_selling = price
+  end
+  def create_markup
+    if self.markup.to_s.include? "%"
+      #convert the percentage to a figure using the buying price
+      #remove the percentage
+      valu = self.markup.to_s.delete "%"
+      amt = (valu.to_f *self.buying_price.to_f)*100
+      self.markup = amt.to_f
+    end
+    
   end
   def capitalize_item_name
     self.item_name.upcase!
